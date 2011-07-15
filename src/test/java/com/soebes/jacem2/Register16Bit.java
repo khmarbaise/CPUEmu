3eaf0775922cd1d60d51c7909a0c385addd134aa
Karl Heinz Marbaise <info@soebes.de>
2011-07-15 16:44:24 +0200

    - Moved Register16Bit, Register8Bit and RegisterSet into   production code area.

--- src/test/java/com/soebes/jacem2/Register16Bit.java
+++
@@ -1,5 +0,0 @@
-package com.soebes.jacem2;
-
-public enum Register16Bit {
-	B, D, H, SP /* I'm not sure about SP in this case? */
-}
--- src/test/java/com/soebes/jacem2/Register8Bit.java
+++
@@ -1,5 +0,0 @@
-package com.soebes.jacem2;
-
-public enum Register8Bit {
-	B, C, D, E, H, L, M, A
-}
